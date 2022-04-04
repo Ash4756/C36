b6ba81c81d3584f74f9a740e5ec77bd5eb468920
Ash4756 <86113463+Ash4756@users.noreply.github.com>
2022-04-04 14:59:44 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,47 @@
+class Form
+{
+    constructor() 
+    {
+        this.input = createInput("").attribute("placeholder","Enter Your Name");
+        this.playButton = createButton("PLAY");
+        this.titleImg = createImg("assets/title.png","Game Title");
+        this.greeting = createElement("h2")
+    } 
+    setelementsposition()
+    {
+        this.input.position(width/2-110,height/2-80);
+        this.playButton.position(width/2-90,height/2-20);
+        this.titleImg.position(120,50);
+        this.greeting.position(width/2-300,height/2-100);
+    }
+    setelementsstyle()
+    {
+        this.input.class("customInput");
+        this.playButton.class("customButton");
+        this.titleImg.class("gameTitle");
+        this.greeting.class("greeting");
+    }
+    hide()
+    {
+        this.greeting.hide();
+        this.playButton.hide();
+        this.input.hide();
+        
+    }
+    handlemousepressed()
+    {
+        this.playButton.mousePressed(()=>{
+        this.input.hide();
+        this.playButton.hide() ;
+        var message = `Hello ${this.input.value()}
+        </br>wait for another player to join....` ;
+        this.greeting.html(message)  
+        })
+    }
+    display()
+    {
+        this.setelementsposition();
+        this.setelementsstyle();
+        this.handlemousepressed();
+    }
+}
